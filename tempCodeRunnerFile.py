7b27589030669aeacd98fb5157818db0aaf0d0d8
AM0312 <ansh.midha0312@gmail.com>
2023-08-21 23:29:41 +0530

    Sorry

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-client = Client(TWILIO_SID, TWILIO_AUTH_KEY)
-    for article in formatted_articles:
-        message = client.messages.create(
-            body=article,
-            to="+918800758061",
-            from_="+18155679217"
-        )
